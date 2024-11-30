f63b5f98784b812668505cadd6e7ad8bbb523bf9
Muhammad Zamrol Imada <imadamada939@gmail.com>
2024-11-30 13:18:42 +0700

    nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       {/* Navbar */}
-      <nav className="flex items-center justify-between bg-white text-black font-bold p-4 sticky top-0 z-10">
-        {/* Icon di sisi kiri */}
-        <div className="flex items-center">
-          <Image src="/icon512.png" alt="Home Icon" width={64} height={96} />
-          <Image src="/icondokterikan.png" alt="Extra Icon" width={164} height={164} />
-        </div>
-
-        {/* Menu di tengah */}
-        <div className="flex space-x-14 text-1xl font-semibold font-sans ml-40 -mr-8">
-          <a href="#beranda" className="hover:underline">Beranda</a>
-          <a href="#artikel" className="hover:underline">Artikel</a>
-          <a href="#riwayat" className="hover:underline">Riwayat</a>
-        </div>
-
-        {/* Button Login */}
-        <button 
-          className="bg-[#69CBF4] text-white px-6 py-1 rounded-lg hover:bg-[#4AABDE] transition text-1x1 font-sans font-semibold mr-8"
-        >
-          Login
-        </button>
-      </nav>
+      <Navbar/>
 
       {/* Main Content */}
       <main
@@ -93,59 +75,7 @@ export default function Home() {
       </main>
 
       {/* Footer */}
-      <footer className="bg-white p-6 text-center font-sans">
-        {/* Kotak persegi panjang dengan background biru untuk membungkus konten */}
-        <div className="bg-[rgba(105,203,244,0.26)] p-8 rounded-lg shadow-lg mx-4">
-          <div className="flex items-center ml-16">
-            <Image src="/icon512.png" alt="Home Icon" width={64} height={96} />
-            <Image src="/icondokterikan.png" alt="Extra Icon" width={164} height={164} />
-          </div>
-
-          {/* Kolom Teks */}
-          <div className="flex justify-center gap-60 mb-4">
-            {/* Kolom pertama */}
-            <div className="text-left ml-12 mt-4">
-              <p className="text-black font-semibold mb-4">Site Map</p>
-              <p className="text-black text-sm mb-2">FAQ</p>
-              <p className="text-black text-sm mb-2">Syarat dan Ketentuan</p>
-              <p className="text-black text-sm">Kebijakan Privasi</p>
-            </div>
-
-            {/* Kolom kedua */}
-            <div className="text-left mt-4">
-              <p className="text-black font-semibold mb-4">Layanan Pengaduan</p>
-              <p className="text-black font-semibold text-sm mb-2">PT Rekayasa Agromarin Indonesia</p>
-              <p className="text-black text-sm mb-2">Jl. H.R. Rasuna Said, Jakarta Selatan</p>
-              <p className="text-black text-sm">help@dokterikan.com/012-3456-7890</p>
-            </div>
-
-            {/* Kolom ketiga */}
-            <div className="text-left -mt-4">
-              {/* Kolaborasi dengan Gambar */}
-              <p className="text-black font-semibold text-sm">Kolaborasi dengan:</p>
-              <div className="flex gap-4 mb-0">
-                <Image src="/cemebsa.png" alt="Partner 1" width={120} height={50} />
-                <Image src="/rai.png" alt="Partner 2" width={50} height={50} />
-              </div>
-
-              {/* Ikon Sosial Media */}
-              <p className="text-black font-semibold -mt-1">Follow Us:</p>
-              <div className="flex gap-4 mb-1">
-                <Image src="/twitter.png" alt="Facebook" width={24} height={24} />
-                <Image src="/instagram.png" alt="Twitter" width={24} height={24} />
-                <Image src="/facebook.png" alt="Instagram" width={24} height={24} />
-                <Image src="/youtube.png" alt="YouTube" width={24} height={24} />
-              </div>
-
-              {/* Tanya Apakah Anda Tenaga Ahli */}
-              <p className="text-black font-semibold text-sm">Apakah Anda Tenaga Ahli Ikan?</p>
-              <button className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-500 transition mt-1">
-                Daftar
-              </button>
-            </div>
-          </div>
-        </div>
-      </footer>
+      <Footer/>
     </div>
   );
 }

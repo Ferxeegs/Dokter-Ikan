7fe5c2988df63abe591849969a281c76d7e30d91
Fadlil Ferdiansyah <fadlilferdy88@gmail.com>
2025-02-13 13:36:13 +0700

    update forgot password

---
+++ backend/models/PasswordResetModel.js
@@ -0,0 +1,38 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/Database.js';
+import User from './UserModel.js';
+
+const PasswordReset = sequelize.define('PasswordReset', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  user_id: {
+    type: DataTypes.INTEGER,  // Ubah dari UUID ke INTEGER
+    allowNull: false,
+    references: {
+      model: User,
+      key: 'user_id'  // Sesuaikan dengan nama kolom di tabel `user`
+    },
+    onDelete: 'CASCADE'
+  },
+  reset_token: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  reset_token_expiry: {
+    type: DataTypes.DATE,
+    allowNull: false
+  }
+}, {
+  tableName: 'password_resets',
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: 'updated_at'
+});
+
+// Hubungkan ke User
+PasswordReset.belongsTo(User, { foreignKey: 'user_id' });
+
+export default PasswordReset;
--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { startRegistration, verifyOtp, completeRegistration } from '../controllers/AuthController.js';
+import { startRegistration, verifyOtp, completeRegistration, forgotPassword, resetPassword } from '../controllers/AuthController.js';
 
 const router = express.Router();
 
 router.post('/start-registration', startRegistration); // Route untuk memulai registrasi
 router.post('/verify-otp', verifyOtp); // Route untuk verifikasi OTP
 router.post('/complete-registration', completeRegistration); // Route untuk melengkapi pendaftaran
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
 
 export default router;

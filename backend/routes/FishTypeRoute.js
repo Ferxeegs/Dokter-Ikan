f4d45b4151274bd12a91447fa23372428b085cd9
Fadlil Ferdiansyah <fadlilferdy88@gmail.com>
2025-02-18 14:38:56 +0700

    update fish detection

--- backend/routes/FishTypeRoute.js
+++ backend/routes/FishTypeRoute.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {
   getAllFishTypes,
   getFishTypeById,
+  getFishTypeByName,
 } from '../controllers/FishTypeController.js';
 
 const router = express.Router();
@@ -9,5 +10,6 @@ const router = express.Router();
 // Definisi route untuk jenis ikan
 router.get('/fish-types', getAllFishTypes);
 router.get('/fish-types/:id', getFishTypeById);
+router.get("/fish/search", getFishTypeByName);
 
 export default router;
